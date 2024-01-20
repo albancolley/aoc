6cb5260ee8eb1f96e9b19b0388441d448415660e
AlbanColley <alban.colley@gmail.com>
2024-01-20 22:24:06 +0000

    2023 23 : Added Graph dataclass, replace PriorityQueue with list

--- aoc2023/23/task.py
+++ aoc2023/23/task.py
@@ -2,10 +2,20 @@
 AOC Day 23
 """
 import sys
+from dataclasses import dataclass, field
 from aoc2023.common.aocbase import AocBase
 from aoc2023.common.setup import configure
-from queue import PriorityQueue
 from collections import defaultdict
+
+
+@dataclass
+class Grid:
+    width: int = 0
+    height: int = 0
+    grid: dict[(int, int), str] = field(default_factory=dict)
+    start: (int, int) = (0, 0)
+    end: (int, int) = (0, 0)
+
 
 class Aoc202323(AocBase):
     """
@@ -23,56 +33,45 @@ class Aoc202323(AocBase):
     walls = ['#']
     spaces = ['.']
 
-    def view(self, grid, width, height, seen=[], padding=0):
+    def view(self, grid, seen=[], padding=0):
         print()
-        for y in range(width):
+        for y in range(grid.width):
             line = ""
-            for x in range(height):
-                if (x,y) in seen:
-                    line += f'{str(seen[(x,y)]):<{padding}}'
+            for x in range(grid.height):
+                if (x, y) in seen:
+                    line += f'{str(seen[(x, y)]):<{padding}}'
                 else:
-                    line += f'{grid[(x,y)]:<{padding}}'
+                    line += f'{grid.grid[(x, y)]:<{padding}}'
             print(line)
         print()
 
-
-    def dfs1(self, grid, start, end):
+    def dfs1(self, grid):
         seen = defaultdict(int)
-        q = PriorityQueue()
-        q.put((0, start, []))
-        while not q.empty():
-            step, position, path = q.get()
-            tile = grid[(position)]
+        q: list = [(0, grid.start, [])]
+        while len(q) > 0:
+            step, position, path = q.pop(0)
+            tile = grid.grid[position]
             next_step = -step + 1
             for move in self.moves[tile]:
                 next_position = (position[0] + move[0], position[1] + move[1])
-                if grid[next_position[0], next_position[1]] in self.walls:
+                if grid.grid[next_position[0], next_position[1]] in self.walls:
                     continue
                 if next_position in path:
                     continue
 
                 if seen[next_position] < next_step:
                     seen[next_position] = next_step
-                    q.put((-next_step, next_position, path + [next_position]))
+                    q.insert(0, (-next_step, next_position, path + [next_position]))
 
         return seen
 
-    def dfs(self, grid, start, end) -> []:
-        q = PriorityQueue()
-        q.put((0, start, [start], True))
+    def dfs(self, grid: Grid, start: (int, int)) -> [((int, int), (int, int), int)]:
+        q: list = [(0, start, [start], True)]
         new_paths = []
-        while not q.empty():
-            step, position, path, skip_first = q.get()
+        while len(q) > 0:
+            step, position, path, skip_first = q.pop(0)
 
-            # if position == end:
-            #     new_paths.append((start, end, len(path)))
-            #     if position not in new_paths:
-            #         new_paths[position] = (len(path), path)
-            #     else:
-            #         new_paths[position] = ( max(new_paths[position][0], len( path)), path)
-            #     continue
-
-            tile = grid[(position)]
+            tile = grid.grid[position]
             next_step = -step + 1
             possibles_moves = []
 
@@ -83,12 +82,12 @@ class Aoc202323(AocBase):
                     move = self.moves[tile][0]
                     next_position = (position[0] + move[0], position[1] + move[1])
                     path.append(next_position)
-                    new_paths.append((start, next_position, len(path)-1))
+                    new_paths.append((start, next_position, len(path) - 1))
                     continue
 
             for move in self.moves[tile]:
                 next_position = (position[0] + move[0], position[1] + move[1])
-                if grid[next_position] in self.walls:
+                if grid.grid[next_position] in self.walls:
                     continue
 
                 if next_position in path:
@@ -97,28 +96,26 @@ class Aoc202323(AocBase):
                 possibles_moves.append(next_position)
 
             for next_position in possibles_moves:
-                q.put((-next_step, next_position, path + [next_position], skip_first))
+                q.insert(0, (-next_step, next_position, path + [next_position], skip_first))
 
         return new_paths
 
-    def calc_1(self, data: dict) -> int:
-        grid, width, height, start, end = data
+    def calc_1(self, grid: Grid) -> int:
         # self.view(grid, width, height)
-        result = self.dfs1(grid, start, end)
+        result = self.dfs1(grid)
 
-        return result[end]
+        return result[grid.end]
 
-    def calc_2(self, data: [str]) -> int:
-        grid, width, height, start, end = data
+    def calc_2(self, grid: Grid) -> int:
 
         # added to simplify the dfs code to look for the second slope.
-        grid[start] = 'v'
-        grid[end[0], end[1]-1] = 'v'
+        grid.grid[grid.start] = 'v'
+        grid.grid[grid.end[0], grid.end[1] - 1] = 'v'
 
-        new_paths: list = self.dfs(grid, start, end)
+        new_paths: list = self.dfs(grid, grid.start)
 
-        #simplify graph to get distances to next slope
-        #simple_path with contain the start and end points between the "top" or "bottom" of a slope
+        # simplify graph to get distances to next slope
+        # simple_path with contain the start and end points between the "top" or "bottom" of a slope
         # i.e. the stars (*) part of example graph below.
         # #.#####################
         # #.......#########...###
@@ -137,17 +134,17 @@ class Aoc202323(AocBase):
                 simple_paths[path[1]] = {}
             simple_paths[path[1]][path[0]] = path[2]
 
-            paths = self.dfs(grid, path[1], end)
+            paths = self.dfs(grid, path[1])
             new_paths += paths
 
         # now search the simple paths for the longest path - have to do them all
         longest_path = 0
-        paths = [(0, start, [start])]
+        paths = [(0, grid.start, [grid.start])]
         while len(paths) > 0:
             length, start, path = paths.pop(0)
             for next_position in simple_paths[start]:
                 new_length = length + simple_paths[start][next_position]
-                if next_position == end:
+                if next_position == grid.end:
                     if longest_path < new_length:
                         longest_path = new_length
                         # print(new_length, path + [next_position])
@@ -156,35 +153,31 @@ class Aoc202323(AocBase):
 
         return longest_path
 
-    def load_handler_part1(self, data: [str]) -> [str]:
-        grid = {}
-        width = len(data[0])
-        height = len(data)
-        start = (0, 0)
-        end= (0,0)
-        for x in range(-1, width + 1):
-            grid[(x, -1)] = self.walls[0]
-        for y in range(0, height):
-            grid[(-1, y)] = self.walls[0]
+    def load_handler_part1(self, data: [str]) -> Grid:
+        grid = Grid(len(data[0]), len(data))
+        for x in range(-1, grid.width + 1):
+            grid.grid[(x, -1)] = self.walls[0]
+        for y in range(0, grid.height):
+            grid.grid[(-1, y)] = self.walls[0]
             line = data[y]
-            for x in range(0, width):
+            for x in range(0, grid.width):
                 if line[x] in self.starts:
                     start = (x, y)
-                    grid[(x, y)] = self.spaces[0]
+                    grid.grid[(x, y)] = self.spaces[0]
                 else:
-                    grid[(x, y)] = line[x]
-            grid[(width, y)] = self.walls[0]
-        for x in range(-1, width + 1):
-            grid[(x, height)] = self.walls[0]
+                    grid.grid[(x, y)] = line[x]
+            grid.grid[(grid.width, y)] = self.walls[0]
+        for x in range(-1, grid.width + 1):
+            grid.grid[(x, grid.height)] = self.walls[0]
 
-        for x in range(width):
-            if grid[(x, 0)] in self.spaces:
-                start = (x, 0)
-            if grid[(x, height-1)] in self.spaces:
-                end = (x, height-1)
+        for x in range(grid.width):
+            if grid.grid[(x, 0)] in self.spaces:
+                grid.start = (x, 0)
+            if grid.grid[(x, grid.height - 1)] in self.spaces:
+                grid.end = (x, grid.height - 1)
 
-        # self.display(grid, width, height)
-        return grid, width, height, start, end
+        # self.view(grid)
+        return grid
 
     def load_handler_part2(self, data: [str]) -> [str]:
         return self.load_handler_part1(data)
